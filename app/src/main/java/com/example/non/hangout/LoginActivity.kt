8a9d3ef52339a0080c8f6d16d8385c0374bb6c69
Non <non9441@gmail.com>
2018-05-24 21:30:56 +0700

    update login page :fearful:

--- app/src/main/java/com/example/non/hangout/LoginActivity.kt
+++ app/src/main/java/com/example/non/hangout/LoginActivity.kt
@@ -3,12 +3,17 @@ package com.example.non.hangout
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.view.View
-import android.widget.EditText
+import com.android.volley.Response.Listener
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.activity_login.*
+import org.json.JSONObject
 
 class LoginActivity : AppCompatActivity() {
+
+    private val REQUEST_URL : String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +21,32 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun onLoginClicked(view: View) {
-        val username = username.text
-        val password = password.text
+        val username = username.text.toString()
+        val password = password.text.toString()
+
+        var listener = Listener<String> {
+            @Override
+            fun onResponse(reponse : String) {
+                var jsonObject = JSONObject(reponse)
+                var success = jsonObject.getBoolean("success")
+
+                if(success) {
+                    var username = jsonObject.getString("username")
+                    var intent = Intent(this,MainActivity::class.java)
+                    intent.putExtra("username", username)
+
+                    this.startActivity(intent)
+                } else {
+                    var builder = AlertDialog.Builder(this)
+                    builder.setMessage("Login Failed").setNegativeButton("Retry", null).create().show()
+                }
+
+            }
+        }
+
+        var loginRequest = LoginRequest(REQUEST_URL,username,password,listener)
+        var requestQueue = Volley.newRequestQueue(this)
+        requestQueue.add(loginRequest)
     }
 
     fun onRegisterClicked(view: View) {
--- app/src/main/java/com/example/non/hangout/RegisterActivity.kt
+++ app/src/main/java/com/example/non/hangout/RegisterActivity.kt
@@ -1,12 +1,18 @@
 package com.example.non.hangout
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.view.View
+import com.android.volley.Response.Listener
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_register.*
+import org.json.JSONObject
 
 class RegisterActivity : AppCompatActivity() {
+
+    private val REQUEST_URL : String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,15 +20,34 @@ class RegisterActivity : AppCompatActivity() {
     }
 
     fun onRegisClicked(view: View) {
-        var username = regusername.text
-        var password = regpassword.text
-        var conpassword = conpassword.text
+        var username = regusername.text.toString()
+        var password = regpassword.text.toString()
+        var conpassword = conpassword.text.toString()
 
         if(!password.equals(conpassword)) {
             var builder = AlertDialog.Builder(this)
             builder.setMessage("Wrong confirmation password").setNegativeButton("Retry", null).create().show()
         } else {
+            var listener = Listener<String> {
+                @Override
+                fun onResponse(reponse: String) {
+                    var jsonObject = JSONObject(reponse)
+                    var success = jsonObject.getBoolean("success")
 
+                    if (success) {
+                        var intent = Intent(this, LoginActivity::class.java)
+                        this.startActivity(intent)
+                    } else {
+                        var builder = AlertDialog.Builder(this)
+                        builder.setMessage("Register Failed").setNegativeButton("Retry", null).create().show()
+                    }
+
+                }
+            }
+
+            var registerRequest = RegisterRequest(REQUEST_URL,username,password,listener)
+            var requestQueue = Volley.newRequestQueue(this)
+            requestQueue.add(registerRequest)
         }
 
     }
